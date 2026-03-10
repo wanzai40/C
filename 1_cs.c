4aa0054016ae6d826dca9e98d695adcd9682369a
折斤 <156317331+wanzai40@users.noreply.github.com>
2026-03-10 19:31:39 +0800

    Add files via upload

---
+++ 1_cs.c
@@ -0,0 +1,17 @@
+/*计算字符长度*/
+
+#include <stdio.h>
+int main(){
+    char arr[] = {"helrrrlo"};
+    
+    int i = 0;
+    while(arr[i]!='\0'){
+        
+            i++;
+        
+      
+    }
+    
+    printf("一共有%d个字符",i);
+    return 0;
+}
